238e3b2eb926435293d9c631376bcaeb6262d5a7
tussingj89 <tussing40@gmail.com>
2021-04-14 21:28:11 -0400

    update

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const passport = require("./passport/setup");
 //imports our login authentication routes
 const auth = require("./routes/auth");
+const routes = require("./routes/api-routes");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -17,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
   }
   
 mongoose.connect
-    (process.env.MONGODB_URI, { useNewUrlParser: true})
+    (process.env.MONGODB_URI || MONGODB_URI , { useNewUrlParser: true})
     .then(console.log(`mongoDB connected at ${MONGODB_URI}`))
     .catch(err => console.log(err));
     // process.env.MONGODB_URI || "mongodb://localhost/Foodies",
@@ -28,19 +29,20 @@ mongoose.connect
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-    session({
-        secret: "very secret this is",
-        resave: false,
-        saveUninitialized: true,
-        store: new MongoStore({ mongooseConnection: mongoose.connection})
-    })
-);
+// app.use(
+//     session({
+//         secret: "very secret this is",
+//         resave: false,
+//         saveUninitialized: true,
+//         store: new MongoStore({ mongooseConnection: mongoose.connection})
+//     })
+// );
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/api/auth", auth);
 // app.get("/", (req, res) => res.send("hello world"));
+app.use(routes);
 
 app.listen(port, () => console.log(
     `example app listening on port ${port}!`

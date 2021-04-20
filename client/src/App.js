9d7aee084bc809d294b2f7a8aa2fe31be347b506
BrienB <barrdom@hotmail.com>
2021-04-19 21:37:01 -0400

    commit before merge with main

--- client/src/App.js
+++ client/src/App.js
@@ -2,32 +2,36 @@ import React from "react";
 // import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
-import PostCard from "./components/PostCard";
-import UserCard from "./components/UserCard";
 import Footer from "./components/Footer";
 // import Home from "./pages/home";
 // import Profile from "./pages/profile";
 // import CreatePost from "./pages/CreatePost";
-
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Login from './components/Login';
+import useToken from './useToken.js';
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
 return (
-    <div>
-        {/* <Router> */}
+    <div className="wrapper">
+        <Router>
             <Nav />
             <Wrapper>
-              <PostCard />
-              {/* <UserCard /> */}
-            {/* <Switch>
-                <Route exact path="/" component={Home} />
+            <Switch>
+                <Route exact path="/" component={Login} />
                 <Route exact path="/home" component={Home} />
-                <Route exact path="/profile" component={Profile} />
-                <Route exact path="/CreatePost" component={CreatePost} />
-            </Switch> */}
+                {/* <Route exact path="/profile" component={Profile} /> */}
+                {/* <Route exact path="/CreatePost" component={CreatePost} /> */}
+            </Switch>
             </Wrapper>
             <Footer />
-        {/* </Router> */}
+        </Router>
     </div>
 );
 }

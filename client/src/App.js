9a09334508dc01c183a4658ff87201a7da6b1dca
BrienB <barrdom@hotmail.com>
2021-04-19 20:35:43 -0400

    changes before pulling Dan's commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 // import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
+import PostCard from "./components/PostCard";
+import UserCard from "./components/UserCard";
 import Footer from "./components/Footer";
 // import Home from "./pages/home";
 // import Profile from "./pages/profile";
@@ -15,6 +17,8 @@ return (
         {/* <Router> */}
             <Nav />
             <Wrapper>
+              <PostCard />
+              {/* <UserCard /> */}
             {/* <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/home" component={Home} />

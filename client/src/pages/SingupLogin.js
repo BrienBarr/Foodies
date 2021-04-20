5723bedafedc5eca9fed3aa43e7d0f625571f5d8
BrienB <barrdom@hotmail.com>
2021-04-19 21:00:38 -0400

    restructured App, Login, and useToken

--- client/src/pages/SingupLogin.js
+++ client/src/pages/SingupLogin.js
@@ -1,1 +1,13 @@
 // create login/signup page import sign up form and login form 
+
+import React from "react";
+import Login from "./src/components/Login";
+
+const SignupLogin = () => {
+    return (
+        <div>
+          <Login/>
+        </div>
+    )
+}
+ export default SignupLogin;
